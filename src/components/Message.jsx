a67217b2e259fbac25a81a5cddeff190ac8baf2f
Arijit Das <arijit10das2002@gmail.com>
2022-10-30 21:59:23 +0530

    message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 export const Message = () => {
   return (
-    <div className='message'>
+    <div className='message owner'>
       <div className="messageInfo">
         <img src='https://images.pexels.com/photos/4883441/pexels-photo-4883441.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt='profilePic'/>
         <span>just now</span>
       </div>
       <div className="messageContent">
         <p>hello</p>
-        {/* <img src='https://images.pexels.com/photos/4883441/pexels-photo-4883441.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt='' /> */}
+        <img src='https://images.pexels.com/photos/4883441/pexels-photo-4883441.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt='' />
       </div>
     </div>
   )
