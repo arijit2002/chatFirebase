33702c8b55f0fc8181c697801d25876eaaf87d54
Arijit Das <arijit10das2002@gmail.com>
2022-10-30 20:06:44 +0530

    chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import Cam from '../img/video-camera.png'
+import Add from '../img/add-user.png'
+import More from '../img/more.png'
 
 export const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className="chatInfo">
+        <span>Jane</span>
+        <div className="chatIcons">
+          <img src={Cam} alt='Camera' />
+          <img src={Add} alt='Add Friend' />
+          <img src={More} alt='More' />
+        </div>
+      </div>
+    </div>
   )
 }
