c2042cf1e23bf1895c5f720915342be5fa9a3107
Arijit Das <arijit10das2002@gmail.com>
2022-10-30 19:07:48 +0530

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import arijit from '../img/arijit.png'
 
 export const Navbar = () => {
   return (
     <div className='navbar'>
       <span className='logo'>ChatApp</span>
       <div className="user">
-        <img src={arijit} alt='' />
+        <img src='https://images.pexels.com/photos/4883441/pexels-photo-4883441.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt='profilePic' />
         <span>Arijit</span>
         <button>Logout</button>
       </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export const Search = () => {
   return (
-    <div>Search</div>
+    <div className='search'>
+      <div className="searchFor">
+        <input type="text" />
+      </div>
+      <div className="userChat">
+        <img src='https://images.pexels.com/photos/4883441/pexels-photo-4883441.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt='' />
+        <div className="userChatInfo">
+          <span>Jane</span>
+        </div>
+      </div>
+    </div>
   )
 }
