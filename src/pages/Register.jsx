162554a71baf2ccd848bea08106786bb17628ce2
Arijit Das <arijit10das2002@gmail.com>
2022-10-30 16:44:19 +0530

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,7 +4,16 @@ export const Register = () => {
   return (
     <div className='formConatainer'>
         <div className="formWrapper">
-            <form action=""></form>
+            <span className='logo'>ChatApp</span>
+            <span className='title'>Register</span>
+            <form>
+                <input type="text" placeholder="display name" />
+                <input type="email" placeholder="email" />
+                <input type="password" placeholder="password" />
+                <input type="file" />
+                <button>Sign up</button>
+            </form>
+            <p>Don't have an account? Login</p>
         </div>
     </div>
   )
