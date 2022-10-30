42a89be0e56fd0b407f0ca5641ce76f280f378d6
Arijit Das <arijit10das2002@gmail.com>
2022-10-30 16:38:58 +0530

    .

---
+++ src/pages/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Register = () => {
+  return (
+    <div className='formConatainer'>
+        <div className="formWrapper">
+            <form action=""></form>
+        </div>
+    </div>
+  )
+}
