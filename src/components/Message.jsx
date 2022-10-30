670d4cd6ca860070ee60169149300a8995876f4b
Arijit Das <arijit10das2002@gmail.com>
2022-10-30 21:18:49 +0530

    message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export const Message = () => {
   return (
-    <div>Message</div>
+    <div className='message'>
+      <div className="messageInfo">
+        <img src='https://images.pexels.com/photos/4883441/pexels-photo-4883441.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt='profilePic'/>
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        {/* <img src='https://images.pexels.com/photos/4883441/pexels-photo-4883441.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt='' /> */}
+      </div>
+    </div>
   )
 }
