2e20326b44bbec4bf017635c3623307e56184cb7
Arijit Das <arijit10das2002@gmail.com>
2022-10-30 18:40:14 +0530

    pic

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import arijit from '../img/arijit.png'
 
 export const Navbar = () => {
   return (
     <div className='navbar'>
       <span className='logo'>ChatApp</span>
       <div className="user">
-        <img src='' alt='' />
+        <img src={arijit} alt='' />
         <span>Arijit</span>
         <button>Logout</button>
       </div>
