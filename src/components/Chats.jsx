625ac2bf1b4aaf7bea321dc4e84652beabd8ef10
Arijit Das <arijit10das2002@gmail.com>
2022-10-30 19:29:04 +0530

    .

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 export const Chats = () => {
   return (
-    <div>Chats</div>
+    <div className='chats'>
+      <div className="userChat">
+        <img src='https://images.pexels.com/photos/4883441/pexels-photo-4883441.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt='' />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
   )
 }
