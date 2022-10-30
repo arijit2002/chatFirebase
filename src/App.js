671f8eff2c428ef5a848cd875d899c56270b90e8
Arijit Das <arijit10das2002@gmail.com>
2022-10-30 17:35:35 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
+
 import "./style.scss"
 
 function App() {
   return (
-    <Register />
+    // <Register />
+    <Login />
   );
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,11 +6,11 @@ export const Login = () => {
     <div className='formConatainer'>
         <div className="formWrapper">
             <span className='logo'>ChatApp</span>
-            <span className='title'>Register</span>
+            <span className='title'>Login</span>
             <form>
                 <input type="email" placeholder="email" />
                 <input type="password" placeholder="password" />
-                <button>Sign In</button>
+                <button>Login</button>
             </form>
             <p>Don't have an account? Sign In</p>
         </div>
