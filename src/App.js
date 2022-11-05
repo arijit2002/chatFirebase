cf79a915308aa55f6a487c70d7e2027395181314
Arijit Das <arijit10das2002@gmail.com>
2022-11-05 22:42:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
 import { Home } from "./pages/Home";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import "./style.scss"
 import { useContext } from "react";
@@ -11,11 +11,21 @@ import { AuthContext } from "./context/AuthContext";
 function App() {
 
   const {currentUser} = useContext(AuthContext);
+  const ProtectedRoute = ({children}) =>{
+    if(!currentUser){
+      return <Navigate to="/login" />;
+    }
+  }
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/">
-          <Route index element={<Home />} />
+          <Route index element={
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        } />
           <Route path="login" index element={<Login />} />
           <Route path="register" index element={<Register />} />
         </Route>
